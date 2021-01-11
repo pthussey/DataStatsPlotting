645a322a3a95a9c514adb211b7d3f5b9dee3db50
pthussey <59669285+pthussey@users.noreply.github.com>
2021-01-12 07:00:02 +0900

    Update dataStatsPlotting.py
    Added docstrings to functions.

--- dataStatsPlotting.py
+++ dataStatsPlotting.py
@@ -8,6 +8,13 @@ import seaborn as sns
 
 # Sets the default rc parameters for plotting
 def SetParams(font='Malgun Gothic', basesize=12, basecolor='0.4'):
+    """Sets some matplotlib rcParams.
+
+    Args:
+        font (str, optional): Choose the font. Defaults to 'Malgun Gothic'.
+        basesize (int, optional): Sets a base font size. Defaults to 12.
+        basecolor (str, optional): Sets a base color. Defaults to '0.4'.
+    """
     plt.rcParams["font.family"] = font
     plt.rcParams["font.size"] = basesize
     plt.rcParams["xtick.labelsize"] = basesize
@@ -27,6 +34,12 @@ def SetParams(font='Malgun Gothic', basesize=12, basecolor='0.4'):
 
 
 def Despine(ax, spines='topright'):
+    """Removes the spines surrounding a plot.
+
+    Args:
+        ax ([type]): The designated axis.
+        spines (str, optional): Can choose 'all' or 'toprightleft'. Defaults to 'topright'.
+    """
     if spines == 'all':
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
@@ -43,10 +56,21 @@ def Despine(ax, spines='topright'):
         ax.spines['top'].set_visible(False)
 
 
-def AnnotateBars(rects, color='0.4', orient='v', offset=3, weight='normal', digits=0, percent=False):
-    """Attaches a text label for each bar in a bar plot. 
+def AnnotateBars(rects, color='0.4', orient='v', offset=3, weight='normal', fontsize=12, digits=0, percent=False):
+    """Adds a text label to each bar in a bar plot. 
     If using seaborn, must save plot to a variable 
-    and pass the patches (ex. g.patches) for the rects parameter."""
+    and pass the patches (ex. g.patches) for the rects parameter.
+
+    Args:
+        rects (matplotib patches): The bars to label.
+        color (str, optional): Label color. Defaults to '0.4'.
+        orient (str, optional): Orientation of the bars to be labelled. Defaults to 'v'.
+        offset (int, optional): Offset from the top / right edge of the bars. Defaults to 3.
+        weight (str, optional): Font weight. Defaults to 'normal'.
+        fontsize (int, optional): Font size. Defaults to 12.
+        digits (int, optional): Number of digits after the decimal point. Defaults to 0.
+        percent (bool, optional): Choose to add percent sign. Defaults to False.
+    """
     if orient == 'h':
         for rect in rects:
             width = rect.get_width()
@@ -59,6 +83,7 @@ def AnnotateBars(rects, color='0.4', orient='v', offset=3, weight='normal', digi
                     xytext=(offset, 0),
                     textcoords="offset points",
                     ha='left', va='center',
+                    fontsize=fontsize,
                     weight=weight, color=color)
     else:
         for rect in rects:
@@ -72,13 +97,23 @@ def AnnotateBars(rects, color='0.4', orient='v', offset=3, weight='normal', digi
                         xytext=(0, offset),
                         textcoords="offset points",
                         ha='center', va='bottom',
+                        fontsize=fontsize,
                         weight=weight, color=color)
 
 
-def AnnotatePointAbove(coord, color='0.4', weight='normal', fontsize=12, ha='center', offset=10, decpoints=1):
-    """Pass coord as an x, y tuple.
-    """
-    form = '{:.' + str(decpoints) + 'f}'
+def AnnotatePointAbove(coord, color='0.4', weight='normal', fontsize=12, ha='center', offset=10, digits=1):
+    """Adds a label above a point. Must pass the coordinate as a tuple.
+
+    Args:
+        coord (tuple): An x,y coordinate.
+        color (str, optional): Label color. Defaults to '0.4'.
+        weight (str, optional): Font weight. Defaults to 'normal'.
+        fontsize (int, optional): Font size. Defaults to 12.
+        ha (str, optional): Horizontal alignment of the label. Defaults to 'center'.
+        offset (int, optional): Offset from the coordinate. Defaults to 10.
+        digits (int, optional): Number of digits after the decimal point. Defaults to 1.
+    """  
+    form = '{:.' + str(digits) + 'f}'
     plt.annotate(form.format(coord[1]),
                  xy=(coord), xytext=(0, offset),
                  textcoords="offset points",
@@ -87,8 +122,19 @@ def AnnotatePointAbove(coord, color='0.4', weight='normal', fontsize=12, ha='cen
                  weight=weight, color=color)
 
 
-def AnnotatePointBelow(coord, color='0.4', weight='normal', fontsize=12, ha='center', offset=10, decpoints=1):
-    form = '{:.' + str(decpoints) + 'f}'
+def AnnotatePointBelow(coord, color='0.4', weight='normal', fontsize=12, ha='center', offset=10, digits=1):
+    """Adds a label below a point. Must pass the coordinate as a tuple.
+
+    Args:
+        coord (tuple): An x,y coordinate.
+        color (str, optional): Label color. Defaults to '0.4'.
+        weight (str, optional): Font weight. Defaults to 'normal'.
+        fontsize (int, optional): Font size. Defaults to 12.
+        ha (str, optional): Horizontal alignment of the label. Defaults to 'center'.
+        offset (int, optional): Offset from the coordinate. Defaults to 10.
+        digits (int, optional): Number of digits after the decimal point. Defaults to 1.
+    """ 
+    form = '{:.' + str(digits) + 'f}'
     plt.annotate(form.format(coord[1]),
                  xy=(coord), xytext=(0, -offset),
                  textcoords="offset points",
@@ -97,8 +143,19 @@ def AnnotatePointBelow(coord, color='0.4', weight='normal', fontsize=12, ha='cen
                  weight=weight, color=color)
 
 
-def AnnotatePointLeft(coord, color='0.4', weight='normal', fontsize=12, va='center', offset=10, decpoints=1):
-    form = '{:.' + str(decpoints) + 'f}'
+def AnnotatePointLeft(coord, color='0.4', weight='normal', fontsize=12, va='center', offset=10, digits=1):
+    """Adds a label to the left of a point. Must pass the coordinate as a tuple.
+
+    Args:
+        coord (tuple): An x,y coordinate.
+        color (str, optional): Label color. Defaults to '0.4'.
+        weight (str, optional): Font weight. Defaults to 'normal'.
+        fontsize (int, optional): Font size. Defaults to 12.
+        ha (str, optional): Horizontal alignment of the label. Defaults to 'center'.
+        offset (int, optional): Offset from the coordinate. Defaults to 10.
+        digits (int, optional): Number of digits after the decimal point. Defaults to 1.
+    """ 
+    form = '{:.' + str(digits) + 'f}'
     plt.annotate(form.format(coord[1]),
                  xy=(coord), xytext=(-offset, 0),
                  textcoords="offset points",
@@ -107,8 +164,19 @@ def AnnotatePointLeft(coord, color='0.4', weight='normal', fontsize=12, va='cent
                  weight=weight, color=color)
 
 
-def AnnotatePointRight(coord, color='0.4', weight='normal', fontsize=12, va='center', offset=10, decpoints=1):
-    form = '{:.' + str(decpoints) + 'f}'
+def AnnotatePointRight(coord, color='0.4', weight='normal', fontsize=12, va='center', offset=10, digits=1):
+    """Adds a label to the right of a point. Must pass the coordinate as a tuple.
+
+    Args:
+        coord (tuple): An x,y coordinate.
+        color (str, optional): Label color. Defaults to '0.4'.
+        weight (str, optional): Font weight. Defaults to 'normal'.
+        fontsize (int, optional): Font size. Defaults to 12.
+        ha (str, optional): Horizontal alignment of the label. Defaults to 'center'.
+        offset (int, optional): Offset from the coordinate. Defaults to 10.
+        digits (int, optional): Number of digits after the decimal point. Defaults to 1.
+    """ 
+    form = '{:.' + str(digits) + 'f}'
     plt.annotate(form.format(coord[1]),
                  xy=(coord), xytext=(offset, 0),
                  textcoords="offset points",
@@ -117,7 +185,18 @@ def AnnotatePointRight(coord, color='0.4', weight='normal', fontsize=12, va='cen
                  weight=weight, color=color)
 
 
-def AnnotateLine(xs, ys, color='0.4', weight='normal', fontsize=12, offset=10, decpoints=1):
+def AnnotateLine(xs, ys, color='0.4', weight='normal', fontsize=12, offset=10, digits=1):
+    """Adds labels to all the points on a line.
+
+    Args:
+        xs (array-like): Sequence of xs
+        ys ([type]): Sequence of ys
+        color (str, optional): Label color. Defaults to '0.4'.
+        weight (str, optional): Font weight. Defaults to 'normal'.
+        fontsize (int, optional): Font size. Defaults to 12.
+        offset (int, optional): Offset from the coordinate. Defaults to 10.
+        digits (int, optional): Number of digits after the decimal point. Defaults to 1.
+    """
     d = dict(zip(range(len(xs)), list(zip(xs,ys)))) # Dict of coordinates
     
     for k, coord in d.items():
@@ -125,32 +204,32 @@ def AnnotateLine(xs, ys, color='0.4', weight='normal', fontsize=12, offset=10, d
             if d[k+1][1] > d[k][1]:
                 AnnotatePointBelow(coord, color=color,
                                    weight=weight,fontsize=fontsize,
-                                   offset=offset, decpoints=decpoints)
+                                   offset=offset, digits=digits)
             else:
                 AnnotatePointAbove(coord, color=color,
                                    weight=weight, fontsize=fontsize,
-                                   offset=offset, decpoints=decpoints)
+                                   offset=offset, digits=digits)
                 
         elif k == len(xs)-1: # Annotate final point in the line
             if d[k-1][1] > d[k][1]:
                 AnnotatePointBelow(coord, color=color,
                                    weight=weight, fontsize=fontsize,
-                                   offset=offset, decpoints=decpoints)
+                                   offset=offset, digits=digits)
             else:
                 AnnotatePointAbove(coord, color=color,
                                    weight=weight, fontsize=fontsize,
-                                   offset=offset, decpoints=decpoints)
+                                   offset=offset, digits=digits)
                 
         else: # Annotate all other points
             if (d[k-1][1] <= d[k][1]) & (d[k+1][1] <= d[k][1]): # Lines form a peak at the point
                 AnnotatePointAbove(coord, color=color,
                                    weight=weight, fontsize=fontsize,
-                                   offset=offset, decpoints=decpoints)
+                                   offset=offset, digits=digits)
                 
             elif (d[k-1][1] > d[k][1]) & (d[k+1][1] > d[k][1]): # Lines form a trough at the point
                 AnnotatePointBelow(coord, color=color,
                                    weight=weight, fontsize=fontsize,
-                                   offset=offset, decpoints=decpoints)
+                                   offset=offset, digits=digits)
                 
             else:
                 # Vectors to use to determine where the annotation needs to go in edge cases
@@ -169,33 +248,33 @@ def AnnotateLine(xs, ys, color='0.4', weight='normal', fontsize=12, offset=10, d
                         print(1, k, np.degrees(in_hangle))
                         AnnotatePointAbove(coord, color=color, ha='right',
                                            weight=weight, fontsize=fontsize,
-                                           offset=offset, decpoints=decpoints)
+                                           offset=offset, digits=digits)
                     else:
                         AnnotatePointLeft(coord, color=color,
                                           weight=weight, fontsize=fontsize,
-                                          offset=offset, decpoints=decpoints)
+                                          offset=offset, digits=digits)
                 
                 elif (d[k-1][1] <= d[k][1]) & (int_angle < 0): # Incoming line slopes up and interior angle is negative
                     if abs(np.degrees(out_hangle)) < 26: # Angle between outgoing line and horizontal is small
                         print(2, k, np.degrees(out_hangle))
                         AnnotatePointBelow(coord, color=color, ha='left',
                                            weight=weight, fontsize=fontsize,
-                                           offset=offset, decpoints=decpoints)
+                                           offset=offset, digits=digits)
                     else:
                         AnnotatePointRight(coord, color=color,
                                            weight=weight, fontsize=fontsize,
-                                           offset=offset, decpoints=decpoints)
+                                           offset=offset, digits=digits)
                         
                 elif (d[k-1][1] >= d[k][1]) & (int_angle >= 0): # Incoming line slopes down and interior angle is positive
                     if abs(np.degrees(out_hangle)) < 26: # Angle between outgoing line and horizontal is small
                         print(3, k, np.degrees(out_hangle))
                         AnnotatePointAbove(coord, color=color, ha='left',
                                            weight=weight, fontsize=fontsize,
-                                           offset=offset, decpoints=decpoints)
+                                           offset=offset, digits=digits)
                     else:
                         AnnotatePointRight(coord, color=color,
                                            weight=weight, fontsize=fontsize,
-                                           offset=offset, decpoints=decpoints)
+                                           offset=offset, digits=digits)
 
 
 def main():
